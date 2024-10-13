8eed1ea018eb8d5129c53740c4c7ed49a7c1c95c
JJiwonYoon <wldnjs0488@gmail.com>
2024-10-13 12:07:17 +0900

    added visualization hpp

--- include/visualization_helpers.hpp
+++ include/visualization_helpers.hpp
@@ -1,37 +1,62 @@
+// visualization_helpers.hpp
+
 #ifndef VISUALIZATION_HELPERS_HPP
 #define VISUALIZATION_HELPERS_HPP
 
 #include "rclcpp/rclcpp.hpp"
 #include "visualization_msgs/msg/marker.hpp"
-#include "visualization_msgs/msg/marker_array.hpp"
-#include "geometry_msgs/msg/point.hpp"
-
-#include <vector>
 #include <memory>
+#include <vector>
 
 class VisualizationHelpers
 {
 public:
-    VisualizationHelpers(rclcpp::Node::SharedPtr node)
+    VisualizationHelpers(rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr marker_publisher)
+        : marker_publisher_(marker_publisher) {}
+
+    void publish_current_position(const std::shared_ptr<double>& odom_x, const std::shared_ptr<double>& odom_y, rclcpp::Clock::SharedPtr clock)
     {
-        path_marker_publisher_ = node->create_publisher<visualization_msgs::msg::Marker>("/visualization_marker_path", 10);
-        position_marker_publisher_ = node->create_publisher<visualization_msgs::msg::Marker>("/visualization_marker_position", 10);
+        visualization_msgs::msg::Marker marker;
+        marker.header.frame_id = "map";
+        marker.header.stamp = clock->now();
+        marker.ns = "current_position";
+        marker.id = 0;
+        marker.type = visualization_msgs::msg::Marker::SPHERE;
+        marker.action = visualization_msgs::msg::Marker::ADD;
+        marker.pose.position.x = *odom_x;
+        marker.pose.position.y = *odom_y;
+        marker.pose.position.z = 0.0;
+        marker.pose.orientation.x = 0.0;
+        marker.pose.orientation.y = 0.0;
+        marker.pose.orientation.z = 0.0;
+        marker.pose.orientation.w = 1.0;
+        marker.scale.x = 0.5;
+        marker.scale.y = 0.5;
+        marker.scale.z = 0.5;
+        marker.color.a = 1.0;
+        marker.color.r = 0.0;
+        marker.color.g = 1.0;
+        marker.color.b = 0.0;
+
+        marker_publisher_->publish(marker);
     }
 
-    void publish_path(const std::shared_ptr<std::vector<double>>& content1, const std::shared_ptr<std::vector<double>>& content2, rclcpp::Time now)
+    void publish_path(const std::shared_ptr<std::vector<double>>& content1,
+                    const std::shared_ptr<std::vector<double>>& content2,
+                    rclcpp::Clock::SharedPtr clock)
     {
-        visualization_msgs::msg::Marker path_marker;
-        path_marker.header.frame_id = "map";
-        path_marker.header.stamp = now;
-        path_marker.ns = "path";
-        path_marker.id = 0;
-        path_marker.type = visualization_msgs::msg::Marker::LINE_STRIP;
-        path_marker.action = visualization_msgs::msg::Marker::ADD;
-        path_marker.scale.x = 0.1;
-        path_marker.color.a = 1.0;
-        path_marker.color.r = 1.0;
-        path_marker.color.g = 0.0;
-        path_marker.color.b = 0.0;
+        visualization_msgs::msg::Marker marker;
+        marker.header.frame_id = "map";
+        marker.header.stamp = clock->now();
+        marker.ns = "path";
+        marker.id = 1;
+        marker.type = visualization_msgs::msg::Marker::LINE_STRIP;
+        marker.action = visualization_msgs::msg::Marker::ADD;
+        marker.scale.x = 0.1;
+        marker.color.a = 1.0;
+        marker.color.r = 1.0;
+        marker.color.g = 0.0;
+        marker.color.b = 0.0;
 
         for (size_t i = 0; i < content1->size(); ++i)
         {
@@ -39,38 +64,14 @@ public:
             p.x = content1->at(i);
             p.y = content2->at(i);
             p.z = 0.0;
-            path_marker.points.push_back(p);
+            marker.points.push_back(p);
         }
 
-        path_marker_publisher_->publish(path_marker);
-    }
-
-    void publish_position(double odom_x, double odom_y, rclcpp::Time now)
-    {
-        visualization_msgs::msg::Marker position_marker;
-        position_marker.header.frame_id = "map";
-        position_marker.header.stamp = now;
-        position_marker.ns = "position";
-        position_marker.id = 1;
-        position_marker.type = visualization_msgs::msg::Marker::SPHERE;
-        position_marker.action = visualization_msgs::msg::Marker::ADD;
-        position_marker.scale.x = 0.5;
-        position_marker.scale.y = 0.5;
-        position_marker.scale.z = 0.5;
-        position_marker.color.a = 1.0;
-        position_marker.color.r = 0.0;
-        position_marker.color.g = 1.0;
-        position_marker.color.b = 0.0;
-        position_marker.pose.position.x = odom_x;
-        position_marker.pose.position.y = odom_y;
-        position_marker.pose.position.z = 0.0;
-
-        position_marker_publisher_->publish(position_marker);
+        marker_publisher_->publish(marker);
     }
 
 private:
-    rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr path_marker_publisher_;
-    rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr position_marker_publisher_;
+    rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr marker_publisher_;
 };
 
 #endif // VISUALIZATION_HELPERS_HPP
--- src/stanley.cpp
+++ src/stanley.cpp
@@ -17,6 +17,7 @@
 #include <utility>
 #include "frenet_optimal_trajectory.hpp"
 #include "cubic_spline_planner.hpp"
+#include "visualization_helpers.hpp"
 
 const double k = 3.0;  // control gain
 const double L = 2.0;  // [m] Wheel base of vehicle
@@ -40,6 +41,7 @@ public:
         cmd_publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
         marker_publisher_ = this->create_publisher<visualization_msgs::msg::Marker>("visualization_marker", 10);
 
+        visual_helpers_ = std::make_shared<VisualizationHelpers>(marker_publisher_);
         odom_x = std::make_shared<double>(0.0);
         odom_y = std::make_shared<double>(0.0);
         current_yaw = std::make_shared<double>(0.0);
@@ -190,60 +192,6 @@ private:
         return {delta, current_target_idx};
     }
 
-    void publish_current_position()
-    {
-        visualization_msgs::msg::Marker marker;
-        marker.header.frame_id = "map";
-        marker.header.stamp = this->now();
-        marker.ns = "current_position";
-        marker.id = 0;
-        marker.type = visualization_msgs::msg::Marker::SPHERE;
-        marker.action = visualization_msgs::msg::Marker::ADD;
-        marker.pose.position.x = *odom_x;
-        marker.pose.position.y = *odom_y;
-        marker.pose.position.z = 0.0;
-        marker.pose.orientation.x = 0.0;
-        marker.pose.orientation.y = 0.0;
-        marker.pose.orientation.z = 0.0;
-        marker.pose.orientation.w = 1.0;
-        marker.scale.x = 0.5;
-        marker.scale.y = 0.5;
-        marker.scale.z = 0.5;
-        marker.color.a = 1.0;
-        marker.color.r = 0.0;
-        marker.color.g = 1.0;
-        marker.color.b = 0.0;
-
-        marker_publisher_->publish(marker);
-    }
-
-    void publish_path()
-    {
-        visualization_msgs::msg::Marker marker;
-        marker.header.frame_id = "map";
-        marker.header.stamp = this->now();
-        marker.ns = "path";
-        marker.id = 1;
-        marker.type = visualization_msgs::msg::Marker::LINE_STRIP;
-        marker.action = visualization_msgs::msg::Marker::ADD;
-        marker.scale.x = 0.1;
-        marker.color.a = 1.0;
-        marker.color.r = 1.0;
-        marker.color.g = 0.0;
-        marker.color.b = 0.0;
-
-        for (size_t i = 0; i < content1->size(); ++i)
-        {
-            geometry_msgs::msg::Point p;
-            p.x = content1->at(i);
-            p.y = content2->at(i);
-            p.z = 0.0;
-            marker.points.push_back(p);
-        }
-
-        marker_publisher_->publish(marker);
-    }
-
     void runWhileLoop()
     {
         std::string filePath1 = "/home/oskar/Downloads/first_x.txt"; // 읽어올 파일 경로
@@ -252,7 +200,7 @@ private:
         int last_target_idx = 0;
         readFileToVectors(filePath1, filePath2, filePath3, content1, content2, content3);
         auto [rx, ry, ryaw, rk, csp] = generate_target_course(*content1, *content2);
-        publish_path();
+        visual_helpers_->publish_path(content1, content2, this->get_clock());
 
         while (rclcpp::ok())
         {
@@ -264,11 +212,12 @@ private:
             cmd_vel_msg.angular.z = delta;
             cmd_publisher_->publish(cmd_vel_msg);
 
-            publish_current_position();
+            visual_helpers_->publish_current_position(odom_x, odom_y, this->get_clock());
 
             std::this_thread::sleep_for(std::chrono::milliseconds(100)); // 100밀리초 대기
         }
     }
+
 
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr carla_odometry;
     rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr carla_object_x;
@@ -281,6 +230,7 @@ private:
     std::shared_ptr<std::vector<double>> content1;
     std::shared_ptr<std::vector<double>> content2;
     std::shared_ptr<std::vector<double>> content3;
+    std::shared_ptr<VisualizationHelpers> visual_helpers_;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_publisher_;
     rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr marker_publisher_;
 };
